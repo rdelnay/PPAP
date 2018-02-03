4b9d8322b6dd46ee35479f8e7ea527e3a5697fca
Ross Delnay <31398690+rdelnay@users.noreply.github.com>
2018-02-02 19:50:01 -0500

    removed comments

--- src/pen/Pen.java
+++ src/pen/Pen.java
@@ -1,9 +1,7 @@
 package pen;
 
 import java.io.PrintStream;
-//import java.util.concurrent.TimeUnit;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
+
 
 /**
  *
@@ -23,13 +21,7 @@ public class Pen {
 		return System.out.printf("I have a %s.\n", a);
 	}
 
-//	public static void printWithDelays(String data, TimeUnit unit, long delay)
-//			throws InterruptedException {
-//		for (char ch : data.toCharArray()) {
-//			System.out.print(ch);
-//			unit.sleep(delay);
-//		}
-//	}
+
 
 	public static void main(String[] args) {
 		String apple = "Apple",
@@ -49,11 +41,7 @@ public class Pen {
 		System.out.println(pineapple + pen);
 		uhh(pp, ap);
 		
-//		try {
-//			printWithDelays("HELLO", TimeUnit.MILLISECONDS, 200);
-//		} catch (InterruptedException ex) {
-//			Logger.getLogger(Pen.class.getName()).log(Level.SEVERE, null, ex);
-//		}
+
 
 	}
 
